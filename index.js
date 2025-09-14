41c67ad3c69c7c1a757cc932f33baf2de3472782
Prince Kibali M <princekibali222@gmail.com>
2025-09-14 06:47:21 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+
+// Sample employee object (for manual testing)
+const employee = {
+  name: "Prrince",
+  streetAddress: "hurlingam"
+};
+
+// 1. Non-destructively update employee
+function updateEmployeeWithKeyAndValue(emp, key, value) {
+  return { ...emp, [key]: value };
+}
+
+// 2. Destructively update employee
+function destructivelyUpdateEmployeeWithKeyAndValue(emp, key, value) {
+  emp[key] = value;
+  return emp;
+}
+
+// 3. Non-destructively delete key from employee
+function deleteFromEmployeeByKey(emp, key) {
+  const newEmployee = { ...emp };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// 4. Destructively delete key from employee
+function destructivelyDeleteFromEmployeeByKey(emp, key) {
+  delete emp[key];
+  return emp;
+}
+
+// Export for testing
+module.exports = {
+  employee,
+  updateEmployeeWithKeyAndValue,
+  destructivelyUpdateEmployeeWithKeyAndValue,
+  deleteFromEmployeeByKey,
+  destructivelyDeleteFromEmployeeByKey
+};
+
